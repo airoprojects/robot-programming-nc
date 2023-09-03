034d6942614bdd11ea1b9663f39d5e270d3ae1c9
BrFranceN <workbrfrancen@gmail.com>
2023-09-03 16:05:07 +0200

    test extrude map

--- gitsetup.sh
+++ gitsetup.sh
@@ -3,7 +3,7 @@
 email='airoprojectsx@gmail.com'
 username='airoprojects'
 repository="robot-programming-nc"
-token="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"
+token="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"
 
 git remote set-url origin https://${token}@github.com/${username}/${repository}
 git remote -v
