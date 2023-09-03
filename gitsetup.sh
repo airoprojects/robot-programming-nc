d33758d37b7256c2adefe17df23a353074ba1f16
airoprojects <132564366+airoprojects@users.noreply.github.com>
2023-09-03 08:27:53 +0000

    new git setup file

---
+++ gitsetup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+email='airoprojectsx@gmail.com'
+username='airoprojects'
+repository="robot-programming-nc"
+token="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"
+
+git remote set-url origin https://${token}@github.com/${username}/${repository}
