1b442188746b9e6ea37cd45c72bc8aafc25c7692
leeoos <leonardo.colosi@gmail.com>
2023-09-03 10:53:33 +0200

    test first push

--- gitsetup.sh
+++ gitsetup.sh
@@ -3,7 +3,7 @@
 email='airoprojectsx@gmail.com'
 username='airoprojects'
 repository="robot-programming-nc"
-token="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"
+token="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"
 
 git remote set-url origin https://${token}@github.com/${username}/${repository}
 git remote -v
