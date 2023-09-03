93cae87458a0f2221552d386621c0dc70be7fb59
BrFranceN <workbrfrancen@gmail.com>
2023-09-03 11:23:00 +0200

    created ws for the project

---
+++ rp_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bitasus/Desktop/Aplha/robot-programming-nc/rp_ws/src
+# Build directory: /home/bitasus/Desktop/Aplha/robot-programming-nc/rp_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
