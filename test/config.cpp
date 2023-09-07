4ba4f6179b08af989b7ee43731f229a156c29404
leeoos <leonardo.colosi@gmail.com>
2023-09-07 18:20:14 +0200

    testing on tets branch

---
+++ test/config.cpp
@@ -0,0 +1,10 @@
+  // Load the configuration file and initialize the simulator 
+  // config confi.json
+  // TODO
+  /*
+    0. Read from json file
+    1. Count numebr of items
+    2. Create a vector of publisher
+    3. Create lauch file to run all the robots nodes:
+        rosrun mrsim robot_node "robot_i" 
+  */
