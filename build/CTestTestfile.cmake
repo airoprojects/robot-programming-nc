70fde1c0ecc4a847306802e3244fe06d3ca447d8
leeoos <leonardo.colosi@gmail.com>
2023-09-07 18:08:32 +0200

    rollback

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leeoos/Projects/robot-programming-nc/rp_ws/src/mrsim
-# Build directory: /home/leeoos/Projects/robot-programming-nc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
