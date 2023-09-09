27853eb69c71c614224674782284d1a89f8aa20f
leeoos <leonardo.colosi@gmail.com>
2023-09-09 03:36:44 +0200

    cleaning

--- rp_ws/build/mrsim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leeoos/Projects/robot-programming-nc/rp_ws/src/mrsim
-# Build directory: /home/leeoos/Projects/robot-programming-nc/rp_ws/build/mrsim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
