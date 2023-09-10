d0288d91c9cb131ac2ae68df7357e1ad41c5b0e9
BrFranceN <workbrfrancen@gmail.com>
2023-09-10 10:03:15 +0200

    updated initSimEnv -> now works fine

--- rp_ws/src/mrsim/bin/robot_node.cpp
+++ rp_ws/src/mrsim/bin/robot_node.cpp
@@ -9,28 +9,17 @@ void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg, *r)
 
 int main(int argc, char **argv) {
 
-  //to do:
-    // read the params from argv (robot)
-    // 
+  ros::init(argc, argv, "robot_node");
+  ros::NodeHandle nh;
 
-  // argv[robot_name, *w]
-  // tmp def of robot
-  
-  IntPoint middle(w.rows/2, w.cols/2);
-  Pose robot_pose;
-  robot_pose.translation = w.grid2world(middle);
-  // create new node ()
-  //Robot r(0.3, &w, robot_pose);
+  // get node namespace
+  std::string namespace;
+  nh.getParam("namespace", namespace);
 
-
-  // Initialize the node with a name, in this case 'cmd_vel_listener'
-  ros::init(argc, argv, "cmd_vel_listener");
-  
-  // Create a node handle to interact with ROS system
-  ros::NodeHandle n;
+  ROS_INFO("Parameter Value: %s", namespace.c_str());
 
   // Create a subscriber object subscribing to the 'cmd_vel' topic, with the callback function 'cmdVelCallback'
-  ros::Subscriber sub = n.subscribe(argv[0]+"cmd_vel", 1000, cmdVelCallback);
+  ros::Subscriber sub = n.subscribe(namespace+"cmd_vel", 1000, cmdVelCallback);
   
   // Continuously pump callbacks until the node is shutdown
   ros::spin();
